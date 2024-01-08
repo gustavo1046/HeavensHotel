a5cbea239b4d796b71803118581a6c4b753e52c7
Gustavo <gustavo1046@hotmail.com>
2024-01-08 18:39:35 -0300

    finazada tela inicial do hotel

--- HeavensHotel/src/app/Components/Pages/home-page/home-page.component.ts
+++ HeavensHotel/src/app/Components/Pages/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../../UsualComponents/nav/nav.component';
 import { PainelComponent } from '../../UsualComponents/painel/painel.component';
 import { RestauranteComponent } from '../../UsualComponents/restaurante/restaurante.component';
+import { LazerComponent } from '../../UsualComponents/lazer/lazer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavComponent, PainelComponent, RestauranteComponent],
+  imports: [NavComponent, PainelComponent, RestauranteComponent, LazerComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
---
+++ HeavensHotel/src/app/Components/UsualComponents/lazer/lazer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lazer',
+  standalone: true,
+  imports: [],
+  templateUrl: './lazer.component.html',
+  styleUrl: './lazer.component.scss'
+})
+export class LazerComponent {
+
+}
