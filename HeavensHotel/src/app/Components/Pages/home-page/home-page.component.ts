c48ce74e54fedc46955f2a3fb674e436fd7691e8
Gustavo <gustavo1046@hotmail.com>
2024-01-08 15:30:23 -0300

    Adicionando component de restaurante

--- HeavensHotel/src/app/Components/Pages/home-page/home-page.component.ts
+++ HeavensHotel/src/app/Components/Pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../UsualComponents/nav/nav.component';
 import { PainelComponent } from '../../UsualComponents/painel/painel.component';
+import { RestauranteComponent } from '../../UsualComponents/restaurante/restaurante.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavComponent, PainelComponent],
+  imports: [NavComponent, PainelComponent, RestauranteComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
