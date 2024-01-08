6a0d67d29b67c386d287f3160a3146b80b59632a
Gustavo <gustavo1046@hotmail.com>
2024-01-08 11:15:03 -0300

    Adicionando elemntos de estilo

---
+++ HeavensHotel/src/app/Components/UsualComponents/restaurante/restaurante.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurante',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurante.component.html',
+  styleUrl: './restaurante.component.scss'
+})
+export class RestauranteComponent {
+
+}
